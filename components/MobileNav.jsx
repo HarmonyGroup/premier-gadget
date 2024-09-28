f9948c681f72820978ec02a77cc2a18ef0e61f53
majid-the-dev <contactmajidoladepo@gmail.com>
2024-09-28 21:39:37 +0100

    Feat: mobile nav test

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -6,21 +6,21 @@ import { RiStore2Line } from "react-icons/ri";
 
 const MobileNav = () => {
   return (
-    <div className="mobile-nav-shadow fixed w-full bottom-0 bg-white py-4 z-[999999] border-t border-gray-100">
+    <div className="mobile-nav-shadow fixed w-full bottom-0 bg-white py-2.5 z-[999999] border-t border-gray-100">
       <div className="grid grid-cols-4">
-        <Link href={"/"} className="col-span-1 flex flex-col items-center gap-1 text-sm">
+        <Link href={"/"} className="col-span-1 flex flex-col items-center gap-1 text-[13px] font-medium">
           <IoMenu size={18} />
           Menu
         </Link>
-        <Link href={"/"} className="col-span-1 flex flex-col items-center gap-1 text-sm">
+        <Link href={"/"} className="col-span-1 flex flex-col items-center gap-1 text-[13px] font-medium">
           <PiShoppingCartSimpleBold size={17} />
           Cart
         </Link>
-        <Link href={"/"} className="col-span-1 flex flex-col items-center gap-1 text-sm">
+        <Link href={"/"} className="col-span-1 flex flex-col items-center gap-1 text-[13px] font-medium">
           <GoPerson size={18} />
           Account
         </Link>
-        <Link href={"/"} className="col-span-1 flex flex-col items-center gap-1 text-sm">
+        <Link href={"/"} className="col-span-1 flex flex-col items-center gap-1 text-[13px] font-medium">
           <RiStore2Line size={18} />
           Outlets
         </Link>
